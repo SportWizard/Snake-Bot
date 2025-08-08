ebdc85fb9a501bb591aebbf70ae2e750356f1bfb
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-08-08 01:32:52 -0400

    Update program

--- snake_bot.py
+++ snake_bot.py
@@ -2,13 +2,13 @@ import numpy as np
 import cv2
 import mss
 import pyautogui
+import pygetwindow
 from PIL import Image
 from pytesseract import pytesseract
+import threading
 
-running = True
 template = cv2.imread("snake.png")
 template2 = cv2.imread("apple.png")
-save_screen = cv2.imread("save.png")
 
 left_wall = 20
 right_wall = left_wall + 580
@@ -30,75 +30,78 @@ def screen_shot(left=0, top=0, width=1920, height=1080):
     })
 
     img = np.array(scr)
-    img = cv2.cvtColor(img, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+
+    img = cv2.resize(img, (width // 2, height // 2))
 
     return img
 
-while running:
-    screen = screen_shot(660, 320, 600, 530)
-
-    #snake
+def snake(screen):
     result = cv2.matchTemplate(screen, template, method)
     min_value, max_value, min_loc, max_loc = cv2.minMaxLoc(result)
 
+    # Use min_loc if the chosen method is TM_SQDIFF or TM_SQDIFF_NORMED
     if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-        location = min_loc
+        snake_loc = min_loc
     else:
-        location = max_loc
+        snake_loc = max_loc
 
-    bottom_template_right = (location[0] + 50, location[1] + 50)
+    return snake_loc
 
-    cv2.rectangle(screen, location, bottom_template_right, (255, 0, 0), 5)
-
-    #apple
-
+def apple(screen):
     result2 = cv2.matchTemplate(screen, template2, method)
     min_value2, max_value2, min_loc2, max_loc2 = cv2.minMaxLoc(result2)
 
+    # Use min_loc if the chosen method is TM_SQDIFF or TM_SQDIFF_NORMED
     if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-        location2 = min_loc2
+        apple_loc = min_loc2
     else:
-        location2 = max_loc2
+        apple_loc = max_loc2
 
-    bottomtemplateRight2 = (location2[0] + 50, location2[1] + 50)
+    return apple_loc
 
-    cv2.rectangle(screen, location2, bottomtemplateRight2, (0, 0, 255), 5)
+def navigation(snake_loc, apple_loc):
+    tolerance = 1
 
-    #calculation
-    print(location, location2)
-    if location[0] > location2[0]:
-        pyautogui.press("a")
-    else:
-        pyautogui.press("d")
+    # Snake
+    if abs(snake_loc[0] - apple_loc[1]) > tolerance:
+        pyautogui.press("a") if snake_loc[0] > apple_loc[0] else pyautogui.press("d")
+    if abs(snake_loc[1] - apple_loc[1]) > tolerance:
+        pyautogui.press("w") if snake_loc[1] > apple_loc[1] else pyautogui.press("s")
 
-    if location[1] > location2[1]:
+    # Boundaries
+    if (snake_loc[0] - left_wall) < wall_warning or (snake_loc[0] - right_wall + 20) > (wall_warning * -1):
         pyautogui.press("w")
-    else:
-        pyautogui.press("s")
+    if (snake_loc[1] - top_wall) < wall_warning or (snake_loc[1] - botton_wall + 20) > (wall_warning * -1):
+        pyautogui.press("a") 
 
-    #boundaries
-    if (location[0] - left_wall) < wall_warning:
-        pyautogui.press("w")
-    elif (location[0] - right_wall + 20) > (wall_warning * -1):
-        pyautogui.press("w")
-    if (location[1] - top_wall) < wall_warning:
-        pyautogui.press("a")
-    elif (location[1] - botton_wall + 20) > (wall_warning * -1):
-        pyautogui.press("a")
+def process_frame(screen):
+    snake_loc = snake(screen)
+    apple_loc = apple(screen)
 
-    #save
-    result3 = cv2.matchTemplate(screen, save_screen, method)
-    min_value3, max_value3, min_loc3, max_loc3 = cv2.minMaxLoc(result3)
+    print(f"Snake:", snake_loc)
+    print(f"Apple:", apple_loc)
 
-    print(max_loc3)
-    if max_loc3 > threshold:
-        pyautogui.press("space")
+    navigation(snake_loc, apple_loc)  
 
-    cv2.imshow("Scanner", screen)
+def main():
+    running = True
+    window_name = "Snake Game — Mozilla Firefox"
 
-    key = cv2.waitKey(25)
+    while running:
+        game_window = pygetwindow.getWindowsWithTitle(window_name)[0]
+        x, y, width, height = game_window.left, game_window.top, game_window.width, game_window.height
 
-    if key == ord("q"):
-        running = False
+        screen = screen_shot(x, y, width, height)
 
-cv2.destroyAllWindows()
+        threading.Thread(target=process_frame, args=(screen,)).start()
+
+        cv2.imshow("Scanner", screen)
+
+        if cv2.waitKey(1) == ord("q"):
+            running = False
+
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
